3d459f5e4aec2eecee8171ab632d1d70482e92d8
Ayman Chabbaki <aymanchabbaki09@gmail.com>
2026-04-22 17:53:28 +0100

    Update API_URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://omnia-gym-backend.vercel.app/api';
 
 export const fetchCategories = async () => {
   const res = await fetch(`${API_URL}/categories`);
